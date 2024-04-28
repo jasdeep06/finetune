83ced4b3882303aff7e40f93926ed0f4aa15660f
Jasdeep Singh Chhabra <jasdeepchhabra94@gmail.com>
2024-04-28 23:16:16 +0530

    cosmetic

--- predict_api.py
+++ predict_api.py
@@ -9,19 +9,15 @@ from fastapi import FastAPI
 app = FastAPI()
 
 model_id = "meta-llama/Llama-2-7b-hf"
-# lora_ckpt_dir_4bit = "4-bit/checkpoint-500"
 lora_ckpt_dir_8bit = "8-bit/checkpoint-500"
 
 model_8bit,tokenizer_8bit = initialize_model(model_id,'8-bit')
-# model_4bit,tokenizer_4bit = initialize_model(model_id,'4-bit')
 peft_model_8bit = PeftModel.from_pretrained(model_8bit,lora_ckpt_dir_8bit)
-# peft_model_4bit = PeftModel.from_pretrained(model_4bit,lora_ckpt_dir_4bit,load_in_4bit=True)
 
 peft_model_8bit = peft_model_8bit.merge_and_unload()
-# peft_model_4bit = peft_model_4bit.merge_and_unload()
 
 @app.post("/generate")
-def read_generate(message: str,model:str):
+def read_generate(message: str):
     t1 = time.time()
     output,num_tokens = generate_output(peft_model_8bit,message,tokenizer_8bit,num_tokens=100)
     time_taken = round(time.time() - t1,2)
