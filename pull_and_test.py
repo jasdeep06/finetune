2fa437d918fe4068a1d6fe306263af52b7fddc0f
Jasdeep Singh Chhabra <jasdeepchhabra94@gmail.com>
2024-04-28 16:03:45 +0530

    pull test code

---
+++ pull_and_test.py
@@ -0,0 +1,11 @@
+from app import generate_output,initialize_model,initialize_dataset,format_prompt
+
+
+model_id = "jasdeep06/llama-7b-samsum"
+print("model id",model_id)
+model,tokenizer = initialize_model(model_id,True)
+train_dataset,_,_ = initialize_dataset("samsum")
+message = format_prompt(train_dataset[50],'')
+output = generate_output(model,message,tokenizer)
+print(output)
+
