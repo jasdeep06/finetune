c428ac476be3fc913bf572091f9d7907685988fa
Jasdeep Singh Chhabra <jasdeepchhabra94@gmail.com>
2024-04-28 22:35:02 +0530

    only 8bit inference

--- predict_api.py
+++ predict_api.py
@@ -9,24 +9,21 @@ from fastapi import FastAPI
 app = FastAPI()
 
 model_id = "meta-llama/Llama-2-7b-hf"
-lora_ckpt_dir_4bit = "4-bit/checkpoint-500"
+# lora_ckpt_dir_4bit = "4-bit/checkpoint-500"
 lora_ckpt_dir_8bit = "8-bit/checkpoint-500"
 
 model_8bit,tokenizer_8bit = initialize_model(model_id,'8-bit')
-model_4bit,tokenizer_4bit = initialize_model(model_id,'4-bit')
-peft_model_8bit = PeftModel.from_pretrained(model_8bit,lora_ckpt_dir_8bit,load_in_8bit=True)
-peft_model_4bit = PeftModel.from_pretrained(model_4bit,lora_ckpt_dir_4bit,load_in_4bit=True)
+# model_4bit,tokenizer_4bit = initialize_model(model_id,'4-bit')
+peft_model_8bit = PeftModel.from_pretrained(model_8bit,lora_ckpt_dir_8bit)
+# peft_model_4bit = PeftModel.from_pretrained(model_4bit,lora_ckpt_dir_4bit,load_in_4bit=True)
 
 peft_model_8bit = peft_model_8bit.merge_and_unload()
-peft_model_4bit = peft_model_4bit.merge_and_unload()
+# peft_model_4bit = peft_model_4bit.merge_and_unload()
 
 @app.post("/generate")
 def read_generate(message: str,model:str):
     t1 = time.time()
-    if model == '8-bit':
-        output = generate_output(peft_model_8bit,message,tokenizer_8bit)
-    elif model == '4-bit':
-        output = generate_output(peft_model_8bit,message,tokenizer_4bit)
+    output = generate_output(peft_model_8bit,message,tokenizer_8bit)
     time_taken = round(time.time() - t1,2)
     return output,time_taken
 
