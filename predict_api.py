575d125aa42316e94fba94dc4a4412b265e08a24
Jasdeep Singh Chhabra <jasdeepchhabra94@gmail.com>
2024-04-28 21:37:26 +0530

    added bits to api

--- predict_api.py
+++ predict_api.py
@@ -15,8 +15,8 @@ lora_ckpt_dir_8bit = "8-bit/checkpoint-500"
 
 model_8bit,tokenizer_8bit = initialize_model(model_id,'8-bit')
 model_4bit,tokenizer_4bit = initialize_model(model_id,'4-bit')
-peft_model_8bit = PeftModel.from_pretrained(model_8bit,lora_ckpt_dir_8bit)
-peft_model_4bit = PeftModel.from_pretrained(model_4bit,lora_ckpt_dir_4bit)
+peft_model_8bit = PeftModel.from_pretrained(model_8bit,lora_ckpt_dir_8bit,load_in_8bit=True)
+peft_model_4bit = PeftModel.from_pretrained(model_4bit,lora_ckpt_dir_4bit,load_in_4bit=True)
 
 peft_model_8bit = peft_model_8bit.merge_and_unload()
 peft_model_4bit = peft_model_4bit.merge_and_unload()
