ceaee06f9761d5b46a1593a98727daa1fb2b7b0a
Jasdeep Singh Chhabra <jasdeepchhabra94@gmail.com>
2024-04-28 15:55:25 +0530

    debug

--- predict_api.py
+++ predict_api.py
@@ -1,6 +1,6 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from typing import Union
-from app import generate_output
+from app import generate_output,initialize_model
 
 from fastapi import FastAPI
 
@@ -8,8 +8,9 @@ app = FastAPI()
 
 model_id = "jasdeep06/llama-7b-samsum"
 print("model id",model_id)
-model = AutoModelForCausalLM.from_pretrained(model_id)
-tokenizer = AutoTokenizer.from_pretrained(model_id)
+# model = AutoModelForCausalLM.from_pretrained(model_id,load_in_8bit=True)
+# tokenizer = AutoTokenizer.from_pretrained(model_id)
+model,tokenizer = initialize_model(model_id,True)
 
 @app.post("/generate")
 def read_generate(message: str):
